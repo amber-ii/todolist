4ca576c2089d8de1a68d11d2f573d46874e2d74a
wei840222 <wei840222@gmail.com>
2020-03-29 11:36:26 +0800

    del bin add test

--- src/test/java/com/todoList/app/AppApplicationTests.java
+++ src/test/java/com/todoList/app/AppApplicationTests.java
@@ -1,13 +1,28 @@
 package com.todoList.app;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AppApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/todos")).andDo(MockMvcResultHandlers.print())
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.content().json("[]"));
 	}
 
 }
